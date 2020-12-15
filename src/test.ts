803b64467e09242a3bc88546118bde45b9f737f0
Adriano Batista <adriano.batista@xgen.com.br>
2020-12-15 19:02:25 -0300

    Criacao de nova branch e alteração de arquivo

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,6 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 // More comment.
+// More and more command
 
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
